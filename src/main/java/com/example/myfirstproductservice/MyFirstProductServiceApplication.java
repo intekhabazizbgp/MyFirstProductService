72d6503129e0ffbee42b284318279971cc0c37af
Aziz <intekhab.aziz@accenture.com>
2024-10-25 14:02:02 +0530

    Initializing Project

---
+++ src/main/java/com/example/myfirstproductservice/MyFirstProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myfirstproductservice/MyFirstProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
